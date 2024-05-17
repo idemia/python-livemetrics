8da821d5a23ab87c6adf03d0979f7c4eafd01fa4
Olivier Heurtier <olivier.heurtier@idemia.com>
2024-05-17 09:23:16 +0200

    Release 0.7.1

--- livemetrics/__init__.py
+++ livemetrics/__init__.py
@@ -164,7 +164,7 @@ import asyncio
 
 from livemetrics.metrics import *
 
-__version__ = '0.8'
+__version__ = '0.7.1'
 __author__ = "Olivier Heurtier"
 __copyright__ = "IDEMIA"
 __license__ = "CeCILL-C"
